0cbc18c95874f8c8c5e18db3ec142a43f71116e6
Nguyen Duc Huy B <nguyen.duc.huyb@sun-asterisk.com>
2020-06-04 22:45:48 +0700

    [Task #1.1] Apply MVVM architecture and RxSwift

---
+++ Zomato_Rx/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  Zomato_Rx
+//
+//  Created by Nguyen Duc Huy on 6/3/20.
+//  Copyright © 2020 nguyen.duc.huyb. All rights reserved.
+//
+
+@import Foundation;
+@import UIKit;
+@import RxSwift;
+@import RxCocoa;
+@import Swinject;
+@import SwinjectStoryboard;
+@import NSObject_Rx;
--- Zomato_Rx/Zomato_Rx/Scenes/Main/ViewController.swift
+++ Zomato_Rx/Zomato_Rx/Scenes/Main/ViewController.swift
@@ -6,15 +6,9 @@
 //  Copyright © 2020 nguyen.duc.huyb. All rights reserved.
 //
 
-import UIKit
-
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-

22b89af21e508f6ebe2265c6a5b38ec3ffb51252
oNguyenDucHuyB <48933364+oNguyenDucHuyB@users.noreply.github.com>
2020-06-06 13:44:58 +0700

    [Task #1.3] Create TabBar (#5)

--- Zomato_Rx/Zomato_Rx/Scenes/Dining/DiningViewController.swift
+++ Zomato_Rx/Zomato_Rx/Scenes/Dining/DiningViewController.swift
@@ -1,12 +1,12 @@
 //
-//  ViewController.swift
+//  DiningViewController.swift
 //  Zomato_Rx
 //
 //  Created by Nguyen Duc Huy B on 5/29/20.
 //  Copyright © 2020 nguyen.duc.huyb. All rights reserved.
 //
 
-final class ViewController: UIViewController {
+final class DiningViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

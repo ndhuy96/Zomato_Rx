85bcb376b36df589ba73705cb4f892443edcbb74
Nguyen Duc Huy B <nguyen.duc.huyb@sun-asterisk.com>
2020-08-02 12:31:22 +0700

    [Task #3.2.1] Create UI for Register Screen

--- Zomato_Rx/Zomato_Rx/Scenes/Register/RegisterViewController.swift
+++ Zomato_Rx/Zomato_Rx/Scenes/Register/RegisterViewController.swift
@@ -7,7 +7,26 @@
 //
 
 final class RegisterViewController: UIViewController {
+    @IBOutlet private weak var backButton: UIBarButtonItem!
+    @IBOutlet weak var nameTextField: AnimatedField!
+    @IBOutlet weak var emailTextField: AnimatedField!
+    @IBOutlet weak var passwordTextField: AnimatedField!
+    @IBOutlet weak var repasswordTextField: AnimatedField!
+    @IBOutlet weak var registerButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        config()
+        setupAnimatedTextField()
+    }
+
+    private func config() {
+        guard let navController = navigationController else { return }
+        navController.navigationBar.shadowImage = UIImage()
+        hideKeyboardWhenTap()
+
+        // RegisterButton
+        registerButton.isEnabled = false
+        registerButton.alpha = 0.3
     }
 }
